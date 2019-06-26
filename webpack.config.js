c9e639d6e9270080fa8c1f7400f15f3fe816c1f0
FrontendMetis <rustamvalyaev@yandex.ru>
2019-06-26 19:44:32 +0300

    added order_DESC

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,32 @@ const historyApiFallback = require('connect-history-api-fallback');
 module.exports = {
   context: path.resolve('src'),
 
-  mode: 'development',
+  mode: 'production',
+
+/*   optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      automaticNameMaxLength: 30,
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }, */
 
   resolve: {
     alias: {
